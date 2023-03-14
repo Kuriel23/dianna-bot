b64e7ee31576f557dc9674e3f19250311e8291f6
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-03-14 23:11:02 +0000

    feat: ...

--- src/util/messages.js
+++ src/util/messages.js
@@ -1,14 +1,15 @@
 module.exports = {
-    giveaway: "🎉 **SORTEIO** 🎉",
-    giveawayEnded: "🎉 **Sorteio Acabou** 🎉",
-    title: '{this.prize}',
-    inviteToParticipate: 'Reaja com 🎉 para participar!',
-    winMessage: 'Parabéns, {winners}! Você ganhou **{this.prize}**!',
-    drawing: 'Sobrando: {timestamp}',
-    dropMessage: 'Seja o primeiro a reagir com 🎉 !',
-    embedFooter: '{this.winnerCount} ganhador(es)',
-    noWinner: 'Sorteio cancelado, sem participantes.',
-    winners: 'Ganhador(es):',
-    endedAt: 'Acaba em',
-    hostedBy: 'Hospedado por: {this.hostedBy}'
+  giveaway:
+    "<@&1025141928729268284> <@&1024518354477785098> <@&1025140139892166739>\n\n🎉 **SORTEIO** 🎉",
+  giveawayEnded: "🎉 **Sorteio Acabou** 🎉",
+  title: "{this.prize}",
+  inviteToParticipate: "Reaja com 🎉 para participar!",
+  winMessage: "Parabéns, {winners}! Você ganhou **{this.prize}**!",
+  drawing: "Sobrando: {timestamp}",
+  dropMessage: "Seja o primeiro a reagir com 🎉 !",
+  embedFooter: "{this.winnerCount} ganhador(es)",
+  noWinner: "Sorteio cancelado, sem participantes.",
+  winners: "Ganhador(es):",
+  endedAt: "Acaba em",
+  hostedBy: "Hospedado por: {this.hostedBy}",
 };
